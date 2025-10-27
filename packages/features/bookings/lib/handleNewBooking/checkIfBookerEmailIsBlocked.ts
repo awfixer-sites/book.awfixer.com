calcom/cal.com

Add BookingValidationService

packages/features/bookings/lib/handleNewBooking/checkIfBookerEmailIsBlocked.ts
@@ -1,13 +1,15 @@
 import { extractBaseEmail } from "@calcom/lib/extract-base-email";
 import { HttpError } from "@calcom/lib/http-error";
-import prisma from "@calcom/prisma";
+import type { UserRepository } from "@calcom/lib/server/repository/user";
 
 export const checkIfBookerEmailIsBlocked = async ({
   bookerEmail,
   loggedInUserId,
+  userRepository,
 }: {
   bookerEmail: string;
   loggedInUserId?: number;
+  userRepository: UserRepository;
 }) => {
   const baseEmail = extractBaseEmail(bookerEmail);
   const blacklistedGuestEmails = process.env.BLACKLISTED_GUEST_EMAILS
@@ -17,37 +19,11 @@ export const checkIfBookerEmailIsBlocked = async ({
   const blacklistedEmail = blacklistedGuestEmails.find(
     (guestEmail: string) => guestEmail.toLowerCase() === baseEmail.toLowerCase()
   );
-
   if (!blacklistedEmail) {
     return false;
   }
 
-  const user = await prisma.user.findFirst({
-    where: {
-      OR: [
-        {
-          email: baseEmail,
-          emailVerified: {
-            not: null,
-          },
-        },
-        {
-          secondaryEmails: {
-            some: {
-              email: baseEmail,
-              emailVerified: {
-                not: null,
-              },
-            },
-          },
-        },
-      ],
-    },
-    select: {
-      id: true,
-      email: true,
-    },
-  });
+  const user = await userRepository.findVerifiedUserByEmail({ email: baseEmail });
 
   if (!user) {
     throw new HttpError({ statusCode: 403, message: "Cannot use this email to create the booking." });

awfixer-sites/book.awfixer.com

packages/features/bookings/lib/handleNewBooking/checkIfBookerEmailIsBlocked.ts
@@ -1,17 +1,19 @@
+import type { UserRepository } from "@calcom/features/users/repositories/UserRepository";
 import { ErrorCode } from "@calcom/lib/errorCodes";
 import { ErrorWithCode } from "@calcom/lib/errors";
 import { extractBaseEmail } from "@calcom/lib/extract-base-email";
-import prisma from "@calcom/prisma";
 import { verifyCodeUnAuthenticated } from "@calcom/trpc/server/routers/viewer/auth/util";
 
 export const checkIfBookerEmailIsBlocked = async ({
   bookerEmail,
   loggedInUserId,
   verificationCode,
+  userRepository,
 }: {
   bookerEmail: string;
   loggedInUserId?: number;
   verificationCode?: string;
+  userRepository: UserRepository;
 }) => {
   const baseEmail = extractBaseEmail(bookerEmail);
 
@@ -23,33 +25,7 @@ export const checkIfBookerEmailIsBlocked = async ({
     (guestEmail: string) => guestEmail.toLowerCase() === baseEmail.toLowerCase()
   );
 
-  const user = await prisma.user.findFirst({
-    where: {
-      OR: [
-        {
-          email: baseEmail,
-          emailVerified: {
-            not: null,
-          },
-        },
-        {
-          secondaryEmails: {
-            some: {
-              email: baseEmail,
-              emailVerified: {
-                not: null,
-              },
-            },
-          },
-        },
-      ],
-    },
-    select: {
-      id: true,
-      email: true,
-      requiresBookerEmailVerification: true,
-    },
-  });
+  const user = await userRepository.findVerifiedUserByEmail({ email: baseEmail });
 
   const blockedByUserSetting = user?.requiresBookerEmailVerification ?? false;
   const shouldBlock = !!blacklistedByEnv || blockedByUserSetting;